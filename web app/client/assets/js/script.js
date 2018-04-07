737391ff308bc1b68d8996ae10349bff2aefff62
Shriya Shekhar <sshekhar@objectiveexperience.com>
2018-04-07 11:19:19 +0800

    HTML files UI

--- web app/client/assets/js/script.js
+++ web app/client/assets/js/script.js
@@ -8,14 +8,48 @@ $(document).ready(function($) {
   
 
   $.ajax({
-    url: 'http://127.0.0.1:5000/streams/neighbors/580',
+    url: 'http://127.0.0.1:5000/streams/neighbors/1104',
     dataType: 'json',
     headers: {"Access-Control-Allow-Origin": '*'},
     crossDomain: true,
     jsonpCallback: 'callback',
     type: 'GET',
     success: function (data) {
-        console.log(data);
+        console.log(data.Target.tags[0],data.Target.tags[1],data.Target.tags[2],data.Target.tags[3]);
+      
+      console.log("Neighbour "+ data.Neighbors[0].tags[0] + data.Target.tags);
+       
+     for (i = 0; i < data.Target.tags.length; i++) {  
+        for (j = 0; j < data.Neighbors.length; j++) {  
+          for (k = 0; k < data.Neighbors[j].tags.length; k++) {  
+            if(data.Target.tags[i] === data.Neighbors[j].tags[k]){
+              var htmltext = $(".related-stream .tags h6 a").html();
+              
+              $('.related-stream .tags').find('h6').each(function() {
+                  console.log($(this).html());
+                if($(this).find('a').html() ===data.Target.tags[i] ){
+                  $(this).css("background-color", "#9b59b6");
+                }
+              });
+              
+              
+              //console.log(htmltext);
+              //$(".related-stream .tags h6").css("background-color", "yellow");
+              console.log("gsdhd");
+            }
+          }
+            
+        }
+     }
+      
+    $('#view-card').click(function(e){
+      $('.card-names').toggle();
+                
+     
+      
+    }); 
+        
+      
     }
 });
   
