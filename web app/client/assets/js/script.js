a499d220e03178166dc3ddf40ef8b96eec818b64
Shriya Shekhar <sshekhar@objectiveexperience.com>
2018-04-22 18:14:16 +0800

    UI - Changes for Page load

--- web app/client/assets/js/script.js
+++ web app/client/assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function($) {
+$( window ).on( "load", function() {
 
 	$('.card__share > a').on('click', function(e){ 
 		e.preventDefault() // prevent default action - hash doesn't appear in url
