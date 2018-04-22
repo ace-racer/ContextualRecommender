97aebd648e6d43fab2ea804c572e57ff1d1f7264
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-04-22 18:54:35 +0800

    Add PS scripts to run server and client

---
+++ web app/util/execute_client_local.ps1
@@ -0,0 +1,2 @@
+invoke-expression 'cmd /c start powershell -Command { set-location "H:\";cd "H:\\TeamStreamz_IW\\code\\web app\\client"; grunt}'
+Start-Process "chrome.exe" "http://127.0.0.1:9000/assets/login.html"
---
+++ web app/util/execute_server_local.ps1
@@ -0,0 +1,1 @@
+python "H:\\TeamStreamz_IW\\code\\web app\\services\\app.py";
