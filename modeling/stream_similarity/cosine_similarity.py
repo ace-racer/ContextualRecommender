472f47597c61929b39a630d672f8b325d8c421ea
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-05-06 23:41:37 +0800

    Add code to compute TF IDF values

--- modeling/stream_similarity/cosine_similarity.py
+++ modeling/stream_similarity/cosine_similarity.py
@@ -1,6 +1,91 @@
+import math
+import pandas as pd
+
 class cosine_similarity:
     def __init__(self):
         pass
 
+    def _get_number_of_tags_for_stream(self, stream_index):
+        """
+        Get the number of tags for a stream - the number of 1's for the tags
+        :param stream_index: The index of the stream
+        :return: The number of tags for the stream
+        """
+        tags_for_stream = self.df.iloc[stream_index]
+        return len(tags_for_stream[tags_for_stream == '1'])
+
+    def _get_number_of_streams_for_tag(self, tag_index):
+        """
+        Get the number of streams for a tag identified by its index
+        :param tag_index:
+        :return:
+        """
+        stream_for_tag = self.df.iloc[:, tag_index]
+        return len(stream_for_tag[stream_for_tag == '1'])
+
+    def _get_term_frequency_for_streams(self, num_streams, num_tags):
+        """
+        Get the term frequency for all the streams
+        :param num_streams: The number of streams
+        :param num_tags: The number of tags
+        :return: A num streams * num tags matrix with the TF of the streams and the tags
+        """
+        tf_value = []
+
+        for stream_index in range(0, num_streams):
+            num_tags_for_stream = _get_number_of_tags_for_stream(stream_index)
+            stream_row = self.df.iloc[stream_index]
+            tf_value_for_a_stream = []
+            for tag_index in range(1, num_tags):
+                if stream_row[tag_index] == '1':
+                    tf_value_for_a_stream.append(float(1.0 / (1.0 + num_tags_for_stream)))
+                else:
+                    tf_value_for_a_stream.append(0)
+            tf_value.append(tf_value_for_a_stream)
+
+        return tf_value
+
+    def _get_inverse_document_frequency_for_tags(self, num_streams, num_tags):
+        """
+        Get the inverse document frequency for the tags
+        :param num_streams:
+        :param num_tags:
+        :return: The IDF values for the tags
+        """
+        idf_value = []
+        for tag_index in range(0, num_tags):
+            idf_value_for_a_tag = math.log(num_streams / (1.0 + self._get_number_of_streams_for_tag(tag_index)))
+            idf_value.append(idf_value_for_a_tag)
+
+        return idf_value
+
+    def compute_tf_idf_for_streams(self, generate_file=False):
+        """
+        Computes the TF IDF values for tge streams and tags and returns a matrix of ||S|| * ||T|| dimension
+        :param generate_file: If the output file needs to be generated
+        :return: a matrix of ||S|| * ||T|| dimension
+        """
+
+        # The rows are the number of streams
+        num_streams = self.df.shape[0]
+
+        # The columns are the number of tags
+        num_tags = self.df.shape[1]
+
+        term_frequency_for_streams = _get_term_frequency_for_streams(num_streams, num_tags)
+        inverse_document_frequencies_for_tags = _get_inverse_document_frequency_for_tags(num_streams, num_tags)
+        tf_idf_values = []
+        for stream_index in range(0, num_streams):
+            tf_idf_values_current_row = term_frequency_for_streams[stream_index]
+            for itr, _ in enumerate(tf_idf_values_current_row):
+                tf_idf_values_current_row[itr] = tf_idf_values_current_row[itr] * inverse_document_frequencies_for_tags[itr]
+
+            tf_idf_values.append(tf_idf_values_current_row)
+
+        return tf_idf_values
+
+
     def compute_similarities_generate_similar_streams(self, stream_tag_frequency_location, similar_streams_generated_file_location):
-        pass
+        self.df = pd.read_csv(stream_tag_frequency_location, header = 0, index_col = 0)
+        tf_idf_values = self.compute_tf_idf_for_streams()
+        print(tf_idf_values)
