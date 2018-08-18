32eec44ec378506068988c5c52dd7697ba537225
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-08-18 14:06:16 +0800

    Update code to update the dataframe that is written to file

--- modeling/tag_generation/pre_processing/language_detection.py
+++ modeling/tag_generation/pre_processing/language_detection.py
@@ -17,16 +17,20 @@ source_file_location = os.path.join(FILES_LOCATION, SOURCE_FILE_NAME)
 input_df = pd.read_csv(source_file_location)
 num_rows = input_df.shape[0]
 
-is_english_initial_values = [True] * num_rows
+is_english_initial_values = [1] * num_rows
 input_df[IS_ENGLISH_CONTENT_COLUMN_NAME] = is_english_initial_values
 
-for _, row in input_df.iterrows():
+
+for idx, row in input_df.iterrows():
     if ENGLISH_IDENTIFIER not in row[LANGUAGE_COLUMN_NAME]:
-        row[IS_ENGLISH_CONTENT_COLUMN_NAME] = False
+        # row[IS_ENGLISH_CONTENT_COLUMN_NAME] = False
+        input_df.loc[idx, IS_ENGLISH_CONTENT_COLUMN_NAME] = 0
     else:
         for other_language_identifier in OTHER_LANGUAGE_IDENTIFIERS:
             if other_language_identifier in row[MODULE_NAME_COLUMN_NAME]:
-                 row[IS_ENGLISH_CONTENT_COLUMN_NAME] = False
+                # row[IS_ENGLISH_CONTENT_COLUMN_NAME] = False
+                input_df.loc[idx, IS_ENGLISH_CONTENT_COLUMN_NAME] = 0
+
 
 target_file_location = os.path.join(FILES_LOCATION, TARGET_FILE_NAME)
 input_df.to_csv(target_file_location, index=False)
