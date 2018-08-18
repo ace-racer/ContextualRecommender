a6a85f3aeaae7884650f2d999c8017d13b3daa00
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-08-18 16:37:18 +0800

    Remove commented code - refer documentation: https://pypi.org/project/rake-nltk/

--- modeling/tag_generation/TagGenerationRake.py
+++ modeling/tag_generation/TagGenerationRake.py
@@ -4,30 +4,6 @@ import constants
 from base_operations import base_operations
 from tag_generation.TagGeneratorBase import TagGeneratorBase
 
-# Uses stopwords for english from NLTK, and all puntuation characters by
-# default
-
-
-# myText = '''The Great Pyramid of Giza (also known as the Pyramid of Khufu or the Pyramid of Cheops) is the oldest and largest of the three pyramids in the Giza pyramid complex bordering what is now El Giza, Egypt. It is the oldest of the Seven Wonders of the Ancient World, and the only one to remain largely intact.
-
-# Based on a mark in an interior chamber naming the work gang and a reference to the fourth dynasty Egyptian Pharaoh Khufu, Egyptologists believe that the pyramid was built as a tomb over a 10- to 20-year period concluding around 2560 BC. Initially at 146.5 metres (481 feet), the Great Pyramid was the tallest man-made structure in the world for more than 3,800 years. Originally, the Great Pyramid was covered by limestone casing stones that formed a smooth outer surface; what is seen today is the underlying core structure. Some of the casing stones that once covered the structure can still be seen around the base. There have been varying scientific and alternative theories about the Great Pyramid's construction techniques. Most accepted construction hypotheses are based on the idea that it was built by moving huge stones from a quarry and dragging and lifting them into place.
-# # Extraction given the text.'''
-
-# r.extract_keywords_from_text(myText)
-
-# print "\n"
-# # To get keyword phrases ranked highest to lowest.
-# print r.get_ranked_phrases()
-# print "\n"
-# # To get keyword phrases ranked highest to lowest with scores.
-# print r.get_ranked_phrases_with_scores()
-
-# print r.get_word_frequency_distribution()
-
-# print r.get_word_degrees()
-
-# print r._generate_phrases(myText)
-
 class RakeTagGenerator(TagGeneratorBase):
     def __init__(self):
         print("The RAKE tag generator will be used. " )
