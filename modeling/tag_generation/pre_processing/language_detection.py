9f7237aba35ccc2195ad3aa88461d80e2be991e7
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-08-18 15:09:06 +0800

    Add code to filters URLs and non English character text details

--- modeling/tag_generation/pre_processing/language_detection.py
+++ modeling/tag_generation/pre_processing/language_detection.py
@@ -1,37 +1,47 @@
 import pandas as pd
 import os
+import re
 
 FILES_LOCATION = "H:\\TeamStreamz_IW\\code\\data\\content"
 SOURCE_FILE_NAME = "card_module_details.csv"
-TARGET_FILE_NAME = "card_module_details_with_eng.csv"
+TARGET_FILE_NAME = "card_module_details_to_process.csv"
 
 ENGLISH_IDENTIFIER = "EN_"
 
-OTHER_LANGUAGE_IDENTIFIERS = ["JP-"]
+UTF_ERROR_PATTERN = "??"
 
-IS_ENGLISH_CONTENT_COLUMN_NAME = "Is English Content"
+SHOULD_PROCESS_ROW = "SHOULD_PROCESS"
 LANGUAGE_COLUMN_NAME = "LANGUAGE"
 MODULE_NAME_COLUMN_NAME = "MODULENAME"
+URL_PATTERN = "([a-zA-Z0-9-_/])+\.[a-z\\n]+$"
+TEXT_DETAILS_COLUMN_NAME = "TEXTDETAILS"
 
 source_file_location = os.path.join(FILES_LOCATION, SOURCE_FILE_NAME)
-input_df = pd.read_csv(source_file_location)
+input_df = pd.read_csv(source_file_location, encoding = "ISO-8859-1")
 num_rows = input_df.shape[0]
 
 is_english_initial_values = [1] * num_rows
-input_df[IS_ENGLISH_CONTENT_COLUMN_NAME] = is_english_initial_values
+input_df[SHOULD_PROCESS_ROW] = is_english_initial_values
 
 
 for idx, row in input_df.iterrows():
-    if ENGLISH_IDENTIFIER not in row[LANGUAGE_COLUMN_NAME]:
-        # row[IS_ENGLISH_CONTENT_COLUMN_NAME] = False
-        input_df.loc[idx, IS_ENGLISH_CONTENT_COLUMN_NAME] = 0
+    text_details = str(row[TEXT_DETAILS_COLUMN_NAME])
+    
+    # if text details is not empty
+    if text_details:
+        # if the text details corresponds to a URL
+        if re.search(URL_PATTERN, text_details):
+            print("{0} is a URL".format(text_details))
+            input_df.loc[idx, SHOULD_PROCESS_ROW] = 0
+        else: 
+            if ENGLISH_IDENTIFIER not in row[LANGUAGE_COLUMN_NAME]:
+                input_df.loc[idx, SHOULD_PROCESS_ROW] = 0
+            else:
+                if UTF_ERROR_PATTERN in text_details:
+                    input_df.loc[idx, SHOULD_PROCESS_ROW] = 0
     else:
-        for other_language_identifier in OTHER_LANGUAGE_IDENTIFIERS:
-            if other_language_identifier in row[MODULE_NAME_COLUMN_NAME]:
-                # row[IS_ENGLISH_CONTENT_COLUMN_NAME] = False
-                input_df.loc[idx, IS_ENGLISH_CONTENT_COLUMN_NAME] = 0
-
+        input_df.loc[idx, SHOULD_PROCESS_ROW] = 0
 
 target_file_location = os.path.join(FILES_LOCATION, TARGET_FILE_NAME)
-input_df.to_csv(target_file_location, index=False)
-print("Output file with the Is English content generated.")
+input_df.to_csv(target_file_location, index=False, encoding = "ISO-8859-1")
+print("Output file with the {0} column has been generated.".format(SHOULD_PROCESS_ROW))
