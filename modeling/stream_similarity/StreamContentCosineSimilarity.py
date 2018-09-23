add3e9ac987e8343ed991820cebd4e725c6c9e7d
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-09-23 22:02:53 +0800

    Add code to generate similarity from TF-IDF of content directly

---
+++ modeling/stream_similarity/StreamContentCosineSimilarity.py
@@ -0,0 +1,13 @@
+import math
+import pandas as pd
+import numpy as np
+import os
+
+import configurations
+import constants
+from base_operations import base_operations
+
+
+class StreamContentCosineSimilarity(base_operations):
+    def __init__(self):
+        pass
